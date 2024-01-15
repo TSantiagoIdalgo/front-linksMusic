4728e88850fcbfdcff6461bc12fa5d8cbc17ae95
TSantiagoIdalgo <tomasidalgo51@gmail.com>
2024-01-15 00:30:46 -0300

    fix:(responsive design)

--- src/components/landing/nav/extendedNav/extendedNav.jsx
+++ src/components/landing/nav/extendedNav/extendedNav.jsx
@@ -3,15 +3,18 @@ import userIcon from '../../../../assets/icons/user-icon.png'
 import Style from './extendedNav.module.css'
 
 const ExtendedNav = () => {
+  const userImg = window.localStorage.getItem('user')
   return (
     <div className={Style.extendedNav}>
         <div className={Style.navLinks}>
-         <NavLink to='#'>Music Player</NavLink>
+         <NavLink to='/musicplayer/home'>Music Player</NavLink>
          <NavLink to='#'>Download</NavLink>
          <NavLink to='#'>About</NavLink>
-         <figure className={Style.userIcon}>
-          <img src={userIcon} alt="user" />
-          </figure>
+         <NavLink to='/login' className={Style.userIcon}>
+          { userImg !== null 
+          ? <img src={userImg} alt='user img' className={Style.user_picture_active}/>
+          : <img src={userIcon} alt='user icon' className={Style.user_picture}/>}
+          </NavLink>
         </div>
     </div>
   )
