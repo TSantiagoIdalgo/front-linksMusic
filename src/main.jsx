fb5e7392f93edb9ccf8e83846db6e2010aeac296
TSantiagoIdalgo <tomasidalgo51@gmail.com>
2024-01-19 19:54:30 -0300

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ const client = new ApolloClient({
     connectToDevTools: true,
     cache: new InMemoryCache(),
     link: new HttpLink({
-      uri: import.meta.env.VITE_API_KEY
+      uri: 'https://back-linksmusic.onrender.com'
     })
 })
 
