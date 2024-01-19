442afb40bcd3c19b5e4a83f3e818645a0e60f863
TSantiagoIdalgo <tomasidalgo51@gmail.com>
2024-01-19 19:59:40 -0300

    fix

--- src/components/user/login/login.jsx
+++ src/components/user/login/login.jsx
@@ -1,15 +1,12 @@
 import Style from './login.module.css'
 import Logo from '../../logo/logo'
 import { useGoogle } from '../../../hooks/network/google/useGoogle'
-import { useFacebook } from '../../../hooks/network/facebook/useFacebook'
 import { Link } from 'react-router-dom'
 import LoginData from './loginData/loginData'
 import GoogleLogin from 'react-google-login'
-import ReactFacebookLogin from 'react-facebook-login'
 
 export default function Login() {
   const { clientId, signIn, signFailure } = useGoogle()
-  const { fbId, success, error } = useFacebook()
 
   return (
     <div className={Style.login}>
@@ -28,16 +25,6 @@ export default function Login() {
             onFailure={signFailure}
             cookiePolicy={'single_host_origin'}
             className={Style.login_google_button}
-          />
-          <ReactFacebookLogin
-            appId={fbId}
-            callback={success}
-            onFailure={error}
-            autoLoad={false}
-            fields="name,email,picture"
-            textButton='Login with Facebook'
-            icon="fa-facebook"
-            cssClass={Style.login_facebook_button}
           />
           <div className={Style.login_middle}/>
           <LoginData/>
