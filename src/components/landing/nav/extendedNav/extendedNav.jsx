7886a556b6140a3666b44e7db66c0c11442af41d
TSantiagoIdalgo <tomasidalgo51@gmail.com>
2024-01-19 21:40:05 -0300

    nav

--- src/components/landing/nav/extendedNav/extendedNav.jsx
+++ src/components/landing/nav/extendedNav/extendedNav.jsx
@@ -10,7 +10,7 @@ const ExtendedNav = () => {
          <NavLink to='/musicplayer/home'>Music Player</NavLink>
          <NavLink to='#'>Download</NavLink>
          <NavLink to='#'>About</NavLink>
-         <NavLink to='/login' className={Style.userIcon}>
+         <NavLink to={`${userImg !== null ? '/musicplayer/home' : '/login'}`} className={Style.userIcon}>
           { userImg !== null 
           ? <img src={userImg} alt='user img' className={Style.user_picture_active}/>
           : <img src={userIcon} alt='user icon' className={Style.user_picture}/>}
